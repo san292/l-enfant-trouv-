43f68843c8de5edbc47b8a20d99141dcb3444766
san man <sance1921@gmail.com>
2022-08-22 18:30:55 +0200

    add phoneMenu and header's responsive problem fixed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
+console.log('process------------------->', process.env.PORT);
 
 const server = app.listen(port, () =>
-	console.log(`App running on port ${port}`)
+  console.log(`App running on port ${port}`)
 );
