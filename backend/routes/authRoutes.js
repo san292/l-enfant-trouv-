0d9828c103b25b9e59bd980db42b26a4d264d83f
Mahfod <mahfod.dev@gmail.com>
2022-08-27 09:59:57 +0200

    add verify-email, optimize models to send new info, add cookies

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,8 @@
-const { register, login } = require('../controllers/authControllers');
+const {
+  register,
+  login,
+  verifyEmail,
+} = require('../controllers/authControllers');
 const { googleSignin } = require('../controllers/googleAuth');
 
 const router = require('express').Router();
@@ -9,5 +13,6 @@ const router = require('express').Router();
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/google').post(googleSignin);
+router.route('/verify-email').post(verifyEmail);
 
 module.exports = router;
--- backend/utils/jwtHelpers.js
+++ backend/utils/jwtHelpers.js
@@ -9,4 +9,27 @@ const createJwt = (payload) => {
 
 const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
-module.exports = { createJwt, isTokenValid };
+const createSendTokenCookies = (user, statusCode, res) => {
+  const token = createJwt(user._id);
+
+  const cookiesOptions = {
+    httpOnly: true,
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+  };
+  if (process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
+  res.cookie('jwt', token, cookiesOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+module.exports = { createJwt, isTokenValid, createSendTokenCookies };
