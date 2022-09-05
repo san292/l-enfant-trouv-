c336943e9a124aeb5ed87712e13bc89e5b4aff46
san man <sance1921@gmail.com>
2022-09-05 14:49:03 +0200

    db connections corrected

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
 
 const Home = () => {
-	const { user } = useSelector((state) => state.user);
-	console.log(user);
-	return <div>Hello {user.name}</div>;
+  const { user } = useSelector((state) => state.user);
+  console.log('hola', user);
+  return <div>Hello {user.name}</div>;
 };
 export default Home;
