9ebbc5f885d41bf1c16e5a87f1fe8aa3d6144a2b
san man <sance1921@gmail.com>
2022-09-15 12:37:34 +0200

    change component signin signup

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,31 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
 import {
-	Home,
-	Programme,
-	Observatoire,
-	NousSoutenir,
-	Mission,
-	Apropos,
-	SeConnecter,
-	Creercompte,
+  Home,
+  Programme,
+  Observatoire,
+  NousSoutenir,
+  Mission,
+  Apropos,
+  SignIn,
+  SignUp
 } from '../pages';
 import VerifyPage from '../pages/Auth/VerifyPage';
 import Login from '../pages/Auth/Login';
 
 const RouteIndex = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='mission' element={<Mission />} />
-			<Route path='programme' element={<Programme />} />
-			<Route path='observatoire' element={<Observatoire />} />
-			<Route path='noussoutenir' element={<NousSoutenir />} />
-			<Route path='apropos' element={<Apropos />} />
-			<Route path='seconnecter' element={<SeConnecter />} />
-			<Route path='creercompte' element={<Creercompte />} />
-			<Route path='verify-email' element={<VerifyPage />} />
-			<Route path='login' element={<Login />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="mission" element={<Mission />} />
+      <Route path="programme" element={<Programme />} />
+      <Route path="observatoire" element={<Observatoire />} />
+      <Route path="noussoutenir" element={<NousSoutenir />} />
+      <Route path="apropos" element={<Apropos />} />
+      <Route path="seconnecter" element={<SignIn />} />
+      <Route path="creercompte" element={<SignUp />} />
+      <Route path="verify-email" element={<VerifyPage />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
+  );
 };
 export default RouteIndex;
