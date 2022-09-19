1f862613a0e6224e9b772e726fbc718b35267968
Mahfod <mahfod.dev@gmail.com>
2022-09-19 16:56:20 +0200

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import { Header } from './components';
 import Routes from './routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import HeaderTest from './components/Header/HeaderTest';
 
 function App() {
 	return (
 		<>
+			<HeaderTest />
 			<BrowserRouter>
 				<Routes />
 				<ToastContainer />
